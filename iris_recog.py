e929ccd789fab2d8db8f45ed185d9a30d42660bc
Nicolò Albergoni <albergoni.nicolo@gmail.com>
2019-05-01 19:30:52 +0200

    Sistemata la struttura del codice, ora è suddiviso in funzioni con parametri modificabili. Aggiunto codice per l'identificazione della pupilla.

--- iris_recog.py
+++ iris_recog.py
@@ -1,24 +1,58 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('iris1.jpg')
-# res = cv2.resize(image, dsize=(500, 500), interpolation=cv2.INTER_CUBIC)
 
-gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-_, thresh = cv2.threshold(gray_img, 100, 255, cv2.THRESH_BINARY)
+def load_image(path, grayscale=False, blur=False):
+    global image
+    global real_image
+    image = cv2.imread(path)
+    real_image = image
 
-circles = cv2.HoughCircles(
-    thresh, cv2.HOUGH_GRADIENT, 1, image.shape[0], param1=30, param2=20, minRadius=0, maxRadius=0)
+    if grayscale is True:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    if blur is True:
+        image = cv2.blur(image, (5, 5))
+        #image = cv2.medianBlur(image, ksize=5)
+        cv2.imshow('Blurred image', image)
 
 
-circles = np.uint16(np.around(circles))
-for i in circles[0, :]:
-    # draw the outer circle
-    cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+def draw_circles(circles, pupil=False):
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
 
-cv2.imshow('Threshold', thresh)
-cv2.imshow('Detected circles', image)
+        if pupil is False:
+            # draw the outer circle
+            cv2.circle(real_image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+            # draw the center of the circle
+            cv2.circle(real_image, (i[0], i[1]), 2, (0, 0, 255), 3)
+        else:
+            cv2.circle(real_image, (i[0], i[1]), i[2], (255, 0, 0), 2)
+            cv2.circle(real_image, (i[0], i[1]), 2, (255, 255, 0), 3)
+
+
+def display_image():
+    cv2.imshow('Detected iris + pupil', real_image)
+
+
+def iris_recognition():
+    _, thresh = cv2.threshold(image, 100, 255, cv2.THRESH_BINARY)
+    circles = cv2.HoughCircles(
+        thresh, cv2.HOUGH_GRADIENT, 1, image.shape[0], param1=30, param2=20, minRadius=0, maxRadius=0)
+    draw_circles(circles)
+
+
+def pupil_recognition():
+    _, thresh = cv2.threshold(image, 20, 255, cv2.THRESH_BINARY)
+    circles = cv2.HoughCircles(
+        thresh, cv2.HOUGH_GRADIENT, 1, image.shape[0], param1=30, param2=20, minRadius=0, maxRadius=0)
+    draw_circles(circles, pupil=True)
+    #cv2.imshow('Threshold pupil', thresh)
+
+
+load_image('./images/iris3.jpg', grayscale=True, blur=True)
+iris_recognition()
+pupil_recognition()
+
+display_image()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
